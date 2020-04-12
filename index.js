e0bcdef2aa4630eec0106b7bf34fcf2d67d68536
Janne Suotsalo <janne.suotsalo@gmail.com>
2020-04-12 15:37:53 +0300

    working chat rooms

--- index.js
+++ index.js
@@ -1,25 +1,35 @@
-'use strict';
+"use strict";
 
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-io.on('connection', (socket) => {
-  console.log('a user connected', socket.id);
-
-  socket.on('disconnect', () => {
-    console.log('a user disconnected', socket.id);
+io.on("connection", (socket) => {
+  console.log("a user connected", socket.id);
+  socket.on("disconnect", () => {
+    console.log("a user disconnected", socket.id);
   });
 
-  socket.on('chat message', (msg) => {
-    console.log('message: ', msg);
-    io.emit('chat message', msg);
+  socket.on("chat message", (msg, room) => {
+    console.log("message: ", msg);
+    console.log(room);
+    io.to(room).emit("chat message", msg);
+  });
+
+  socket.on("change room", (room) => {
+    console.log("changing to room: ", room);
+    socket.join(room);
+  });
+
+  socket.on("leave room", (room) => {
+    console.log("left a room: ", room);
+    socket.leave(room);
   });
 });
 
 http.listen(3000, () => {
-  console.log('listening on port 3000');
+  console.log("listening on port 3000");
 });
--- public/chat.js
+++ public/chat.js
@@ -1,16 +1,58 @@
-'use strict';
+"use strict";
 
 const socket = io();
+let room = "";
+let username;
 
-document.querySelector('form').addEventListener('submit', (event) => {
-  event.preventDefault();
-  const inp = document.getElementById('m');
-  socket.emit('chat message', inp.value);
-  inp.value = '';
+function addName() {
+  username = prompt("Please enter your name:", "");
+  if (username == null || username == "") {
+    alert("you did not add a name, defaulting name to User");
+    username = "User";
+  }
+}
+
+document.querySelector("form").addEventListener("submit", (event) => {
+  if (room == "") {
+    alert("Choose a room to start chatting");
+    event.preventDefault();
+  } else {
+    event.preventDefault();
+    const inp = document.getElementById("m");
+    const message = username + ": " + inp.value;
+    socket.emit("chat message", message, room);
+    inp.value = "";
+  }
 });
 
-socket.on('chat message', (msg) => {
-  const item = document.createElement('li');
+const changeRoom = (element) => {
+  socket.emit("leave room", room);
+
+  //user leave msg
+  if (room != element.innerHTML) {
+    let userChangeRoomMessage =
+      "--------" +
+      username +
+      " switched to room: " +
+      element.innerHTML +
+      "--------";
+    socket.emit("chat message", userChangeRoomMessage, room);
+  }
+
+  // user join msg
+  if (room != element.innerHTML) {
+    let userChangeRoomMessage =
+      "--------" + username + " joined room: " + element.innerHTML + "--------";
+    socket.emit("chat message", userChangeRoomMessage, element.innerHTML);
+  }
+
+  room = element.innerHTML;
+  document.getElementById("roomTitle").innerHTML = "Current room: " + room;
+  socket.emit("change room", room);
+};
+
+socket.on("chat message", (msg) => {
+  const item = document.createElement("li");
   item.innerHTML = msg;
-  document.getElementById('messages').appendChild(item);
+  document.getElementById("messages").appendChild(item);
 });
